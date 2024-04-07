8b01ebba49736447504f6fbbde0fc23016b07b91
abellamontoya <150361278+abellamontoya@users.noreply.github.com>
2024-04-07 18:52:00 +0200

    HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,16 @@
+/**
+ * Aquesta és la classe HelloWorld per mostrar una salutació.
+ */
 public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hola, món!!");
+
+    /**
+     * Aquest mètode imprimeix una salutació amb el nom proporcionat.
+     *
+     * @param nom El nom del destinatari de la salutació.
+     * @return La salutació completa.
+     */
+    public String saluda(String nom) {
+        return "Hola, " + nom + "!";
     }
+
 }
